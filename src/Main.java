887ebf8aeab0b48e1368e95c66d9b93002f7820d
sofia va <sophia.lily.lee@gmail.com>
2024-08-15 10:44:51 +0300

    bonus miles

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int ticketCost = 11_543;
+        int oneBonusMile = 20;
+
+        int bonusMiles = ticketCost / oneBonusMile;
+
+        System.out.println(bonusMiles);
     }
 }
